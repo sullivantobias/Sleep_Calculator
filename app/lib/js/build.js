7f927423d0b8c4b207ee9c2488a90e7822028056
Sullivan <stobias@sqli.com>
2018-11-23 16:23:18 +0100

    Finished logic

--- app/lib/js/build.js
+++ app/lib/js/build.js
@@ -1,2 +1,70 @@
-console.log('test');
+/**
+ * @class Calculator
+ */
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+        this.chosenTime = [];
+        this.selects = document.querySelectorAll('select');
+        this.send = document.querySelector('button');
+        this.cycles = {
+            best: {
+                hours: 9,
+                minutes: 0
+            },
+            better: {
+                hours: 7,
+                minutes: 30
+            },
+            middle: {
+                hours: 6,
+                minutes: 0
+            },
+            bad: {
+                hours: 4,
+                minutes: 30
+            }
+        };
+        this.bestTimes = [];
+    }
+    Calculator.prototype.retrieveEachValue = function () {
+        var _this = this;
+        this.chosenTime = [];
+        this.selects.forEach(function (el) { return _this.chosenTime.push(parseInt(el.value)); });
+    };
+    Calculator.prototype.insertAfter = function (newNode, referenceNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    };
+    Calculator.prototype.times = function (h, m) {
+        var hours = this.chosenTime[0];
+        var minutes = this.chosenTime[1];
+        var mm = moment();
+        var times = (mm.hours(hours), mm.minute(minutes));
+        var time = (times.subtract(h, 'hour'), times.subtract(m, 'minute'));
+        time = time.format('HH:mm');
+        this.bestTimes.push(time);
+    };
+    Calculator.prototype.calc = function () {
+        var _this = this;
+        this.send.addEventListener('click', function () {
+            _this.retrieveEachValue();
+            _this.bestTimes = [];
+            _this.times(_this.cycles.best.hours, _this.cycles.best.minutes);
+            _this.times(_this.cycles.better.hours, _this.cycles.better.minutes);
+            _this.times(_this.cycles.middle.hours, _this.cycles.middle.minutes);
+            _this.times(_this.cycles.bad.hours, _this.cycles.bad.minutes);
+            console.log(_this.bestTimes);
+            var form = document.querySelector('.form');
+            var ul = document.createElement('ul');
+            for (var i = 0; i < _this.bestTimes.length; i++) {
+                var li = document.createElement('li');
+                li.innerHTML = _this.bestTimes[i];
+                ul.append(li);
+            }
+            _this.insertAfter(ul, form);
+        });
+    };
+    return Calculator;
+}());
+var test = new Calculator();
+test.calc();
 //# sourceMappingURL=build.js.map
--- src/logic/calculator.ts
+++ src/logic/calculator.ts
@@ -1,1 +1,90 @@
-console.log('test');
+/**
+ * @class Calculator
+ */
+
+class Calculator {
+  private chosenTime: Array<number>;
+  private selects: NodeListOf<HTMLSelectElement>;
+  private send: HTMLElement;
+
+  private cycles;
+
+  private bestTimes: Array<any>;
+
+  constructor() {
+    this.chosenTime = [];
+    this.selects = document.querySelectorAll('select');
+    this.send = document.querySelector('button');
+
+    this.cycles = {
+      best: {
+        hours: 9,
+        minutes: 0,
+      },
+      better: {
+        hours: 7,
+        minutes: 30,
+      },
+      middle: {
+        hours: 6,
+        minutes: 0,
+      },
+      bad: {
+        hours: 4,
+        minutes: 30,
+      },
+    };
+    this.bestTimes = [];
+  }
+
+  private retrieveEachValue(): void {
+    this.chosenTime = [];
+    this.selects.forEach((el) => this.chosenTime.push(parseInt(el.value)));
+  }
+
+  private insertAfter(newNode: HTMLElement, referenceNode: Element) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+
+  private times(h: number, m: number) {
+    const hours = this.chosenTime[0];
+    const minutes = this.chosenTime[1];
+
+    const mm = moment();
+
+    const times = (mm.hours(hours), mm.minute(minutes));
+
+    let time = (times.subtract(h, 'hour'), times.subtract(m, 'minute'));
+    time = time.format('HH:mm');
+    this.bestTimes.push(time);
+  }
+
+  public calc(): void {
+    this.send.addEventListener('click', () => {
+      this.retrieveEachValue();
+
+      this.bestTimes = [];
+
+      this.times(this.cycles.best.hours, this.cycles.best.minutes);
+      this.times(this.cycles.better.hours, this.cycles.better.minutes);
+      this.times(this.cycles.middle.hours, this.cycles.middle.minutes);
+      this.times(this.cycles.bad.hours, this.cycles.bad.minutes);
+
+      console.log(this.bestTimes);
+
+      const form = document.querySelector('.form');
+      const ul = document.createElement('ul');
+
+      for (let i = 0; i < this.bestTimes.length; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = this.bestTimes[i];
+        ul.append(li);
+      }
+
+      this.insertAfter(ul, form);
+    });
+  }
+}
+
+const test = new Calculator();
+test.calc();
