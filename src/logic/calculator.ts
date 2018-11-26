05f39bb2ad6c8eaa4212e5f2a13f3bb8ebb4855b
Sullivan <githubst@gmail.com>
2018-11-26 17:20:39 +0100

    Adding another option

--- src/logic/calculator.ts
+++ src/logic/calculator.ts
@@ -3,18 +3,41 @@
  */
 
 class Calculator {
-  private chosenTime: Array<number>;
-  private selects: NodeListOf<HTMLSelectElement>;
-  private send: HTMLElement;
+  private chosenTimeWake: Array<number>;
+  private chosenTimeSleep: Array<number>;
+
+  private selectsWake: NodeListOf<HTMLSelectElement>;
+  private selectsSleep: NodeListOf<HTMLSelectElement>;
+
+  private sendWake: HTMLInputElement;
+  private sendSleep: HTMLInputElement;
+
+  private resetWake: HTMLInputElement;
+  private resetSleep: HTMLInputElement;
+
+  private switch: HTMLInputElement;
 
   private cycles;
 
   private bestTimes: Array<any>;
 
+  private activate: boolean;
+
   constructor() {
-    this.chosenTime = [];
-    this.selects = document.querySelectorAll('select');
-    this.send = document.querySelector('button');
+    this.chosenTimeWake = [];
+    this.chosenTimeSleep = [];
+
+    this.selectsWake = document.querySelectorAll('#wake > select');
+    this.selectsSleep = document.querySelectorAll('#sleep > select');
+    this.switch = document.querySelector('input[type="checkbox"]');
+
+    this.sendWake = document.querySelector('#sendWake');
+    this.sendSleep = document.querySelector('#sendSleep');
+
+    this.resetWake = document.querySelector('#resetWake');
+    this.resetSleep = document.querySelector('#resetSleep');
+
+    this.activate = false;
 
     this.cycles = {
       best: {
@@ -37,9 +60,33 @@ class Calculator {
     this.bestTimes = [];
   }
 
+  private init() {
+    this.selectsWake[0].value = this.selectsSleep[0].value = 'hours';
+    this.selectsWake[1].value = this.selectsSleep[1].value = 'minutes';
+  }
+
+  private checkValue(
+      selects: NodeListOf<HTMLSelectElement>,
+      arrayToFill: Array<number>
+  ): void {
+    selects.forEach((element) => {
+      if (element.value === ('hours' || 'minutes')) {
+        alert('choose a time');
+      } else {
+        arrayToFill.push(parseInt(element.value));
+      }
+    });
+  }
+
   private retrieveEachValue(): void {
-    this.chosenTime = [];
-    this.selects.forEach((el) => this.chosenTime.push(parseInt(el.value)));
+    this.chosenTimeWake = [];
+    this.chosenTimeSleep = [];
+
+    if (!this.activate) {
+      this.checkValue(this.selectsWake, this.chosenTimeWake);
+    } else {
+      this.checkValue(this.selectsSleep, this.chosenTimeSleep);
+    }
   }
 
   private insertAfter(newNode: HTMLElement, referenceNode: Element) {
@@ -47,33 +94,50 @@ class Calculator {
   }
 
   private times(h: number, m: number) {
-    const hours = this.chosenTime[0];
-    const minutes = this.chosenTime[1];
+    const wake = moment();
+    const sleep = moment();
 
-    const mm = moment();
+    const hoursWake = this.chosenTimeWake[0];
+    const minutesWake = this.chosenTimeWake[1];
 
-    const times = (mm.hours(hours), mm.minute(minutes));
+    const hoursSleep = this.chosenTimeSleep[0];
+    const minutesSleep = this.chosenTimeSleep[1];
 
-    let time = (times.subtract(h, 'hour'), times.subtract(m, 'minute'));
-    time = time.format('HH:mm');
-    this.bestTimes.push(time);
+    const timesWake = (wake.hours(hoursWake), wake.minute(minutesWake));
+    const timesSleep = (sleep.hours(hoursSleep), sleep.minute(minutesSleep));
+
+    /** time to get to sleep */
+    if (this.chosenTimeWake.length !== 0) {
+      let timeWake = (timesWake.subtract(h, 'hour'),
+      timesWake.subtract(m, 'minute'));
+      timeWake = timeWake.format('HH:mm');
+      this.bestTimes.push(timeWake);
+    }
+
+    /** time sto wake up */
+    if (this.chosenTimeSleep.length !== 0) {
+      let timeSleep = (timesSleep.add(h, 'hour'),
+      timesSleep.subtract(m, 'minute'));
+      timeSleep = timeSleep.format('HH:mm');
+      this.bestTimes.push(timeSleep);
+    }
   }
 
-  public calc(): void {
-    this.send.addEventListener('click', () => {
+  private sendTimes(button: Element, ...cycles: Array<number>): void {
+    button.addEventListener('click', () => {
       this.retrieveEachValue();
 
       this.bestTimes = [];
 
-      this.times(this.cycles.best.hours, this.cycles.best.minutes);
-      this.times(this.cycles.better.hours, this.cycles.better.minutes);
-      this.times(this.cycles.middle.hours, this.cycles.middle.minutes);
-      this.times(this.cycles.bad.hours, this.cycles.bad.minutes);
+      this.times(cycles[0], cycles[1]);
+      this.times(cycles[2], cycles[3]);
+      this.times(cycles[4], cycles[5]);
+      this.times(cycles[6], cycles[7]);
 
-      console.log(this.bestTimes);
+      const form: Element = document.querySelector('.row');
+      const ul: HTMLElement = document.createElement('ul');
 
-      const form = document.querySelector('.form');
-      const ul = document.createElement('ul');
+      ul.classList.add('bestTimes');
 
       for (let i = 0; i < this.bestTimes.length; i++) {
         const li = document.createElement('li');
@@ -83,6 +147,74 @@ class Calculator {
 
       this.insertAfter(ul, form);
     });
+  }
+
+  private resetTimes(...button: Array<Element>): void {
+    button.forEach((b) => {
+      b.addEventListener('click', () => this.init());
+    });
+  }
+
+  private switchActive() {
+    this.switch.addEventListener('change', () => {
+      if (this.switch.checked) {
+        this.selectsSleep.forEach((element) => (element.disabled = false));
+        this.sendSleep.disabled = this.resetSleep.disabled = false;
+
+        this.selectsWake.forEach(
+            (element) => ((element.disabled = true), this.init())
+        );
+        this.sendWake.disabled = this.resetWake.disabled = true;
+
+        this.activate = true;
+
+        document.querySelector('ul').remove();
+      } else {
+        this.selectsSleep.forEach(
+            (element) => ((element.disabled = true), this.init())
+        ),
+        (this.sendSleep.disabled = this.resetSleep.disabled = true);
+
+        this.selectsWake.forEach((element) => (element.disabled = false));
+        this.sendWake.disabled = this.resetWake.disabled = false;
+
+        this.activate = false;
+
+        document.querySelector('ul').remove();
+      }
+    });
+  }
+
+  public calc(): void {
+    this.bestTimes = [];
+
+    this.switchActive();
+
+    this.resetTimes(this.resetSleep, this.resetWake);
+
+    this.sendTimes(
+        this.sendWake,
+        this.cycles.best.hours,
+        this.cycles.best.minutes,
+        this.cycles.better.hours,
+        this.cycles.better.minutes,
+        this.cycles.middle.hours,
+        this.cycles.middle.minutes,
+        this.cycles.bad.hours,
+        this.cycles.bad.minutes
+    );
+
+    this.sendTimes(
+        this.sendSleep,
+        this.cycles.best.hours,
+        this.cycles.best.minutes,
+        this.cycles.better.hours,
+        this.cycles.better.minutes,
+        this.cycles.middle.hours,
+        this.cycles.middle.minutes,
+        this.cycles.bad.hours,
+        this.cycles.bad.minutes
+    );
   }
 }
 
